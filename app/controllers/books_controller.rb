418ce32e108b3c176c4be8759607c02c5f9a95e5
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-06-29 10:20:27 +0000

    [update] ページレイアウト整理,[add]show.editページ作成、show.edit.update機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,21 +16,28 @@ class BooksController < ApplicationController
 
   def index
     @books=Book.all
-    @book=Book.new(book_params)
+    @book=Book.new
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
 
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
 
 
   private
 
   def book_params
-  params.permit(:title,:body)
+  params.require(:book).permit(:title,:body)
   end
 
 
